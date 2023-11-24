1ae0be52be4d6bf9ab78ae399601b5f70dc1c819
王斌 <253498229@qq.com>
2023-11-24 17:18:48 +0800

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    NgZorroAntdMobileModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+}
